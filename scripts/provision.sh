5ccbcabacf78c04f778281fb07b5217e9cbc4686
d-a-t-a-d-i-t <d-a-t-a-d-i-t@users.noreply.github.com>
2015-10-06 16:34:55 +0200

    Initial commit of provision and MT4 install scripts.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -18,22 +18,17 @@ sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
 sudo dpkg-reconfigure debconf -f noninteractive -p critical
 
 # Install and run X virtual framebuffer.
-sudo apt-get install -y Xvfb xdotool
+sudo apt-get install -y Xvfb xdotool winbind git
+Xvfb :9 -screen 9 1024x768x16 &
 
 # Install wine
 sudo dpkg --add-architecture i386
-sudo add-apt-repository -y ppa:ubuntu-wine
+sudo add-apt-repository -y ppa:ubuntu-wine/ppa
 sudo apt-get update
-sudo apt-get install -y wine
+sudo apt-get install -y wine1.7
 
-# Upgrade manually some packages from the source.
-sudo apt-get install -y libx11-dev libxtst-dev libxinerama-dev libxkbcommon-dev
-
-# Install xdotool.
-git clone https://github.com/jordansissel/xdotool && make -C xdotool
-
-# Run X virtual framebuffer on screen 0.
-Xvfb :0 -screen 0 1024x768x16 & # Run X virtual framebuffer on screen 0.
+# Workaround for bad GNU TLS linking in Wine
+ln -s /usr/lib/i386-linux-gnu/libgnutls-deb0.so.28 /usr/lib/i386-linux-gnu/libgnutls.so.26
 
 # Give vagrant write permission for /opt.
 sudo chown -R vagrant:vagrant /opt
